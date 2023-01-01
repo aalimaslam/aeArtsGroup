7b14b90cbbf36663bbd017d070cf9f2dea3f25c6
Aalim aslam <helloguys13579@gmail.com>
2023-01-01 12:51:31 -0800

    added subtle animation in background

--- script.js
+++ script.js
@@ -8,5 +8,23 @@ if(window.location.pathname == "/about.html"){
 }
 setTimeout(loader, 1000);
 
-console.log(window.location.href)
-console.log(window.location.pathname)
+let imagesName = ["circle.png","triangle.png","twist.png","X.png"];
+let totalHeight = document.documentElement.offsetHeight;
+function background(){
+    for(let i = 0; i < 40; i++){
+        let img = document.createElement("img");
+        img.src = "./imgs/" + imagesName[Math.floor(Math.random() * imagesName.length)];
+        img.style.width = "40px";
+        img.style.height = "40px";
+        img.style.position = "absolute";
+        img.style.opacity = "0.2";
+        img.style.animation = "animate 10s alternate infinite"
+        img.style.top = Math.floor(Math.random() * totalHeight) + "px";
+        img.style.left = Math.floor(Math.random() * 100) + "%";
+        img.style.zIndex = "-1";
+        document.body.appendChild(img);
+    }
+}
+background();
+
+console.log(document.documentElement.offsetHeight);
